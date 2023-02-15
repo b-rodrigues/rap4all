fbd15626ee82ec0197aed58eeb2e730b2deec803
Bruno Rodrigues <bruno.rodrigues@mesr.etat.lu>
2023-02-15 19:20:45 +0100

    intro

--- auto/references.el
+++ auto/references.el
@@ -6,6 +6,11 @@
     "chambers2014"
     "wickham2019"
     "leisch2002"
-    "xie2014"))
+    "xie2014"
+    "arel22"
+    "gohel23"
+    "xie2020r"
+    "hammant2020"
+    "trisovic2022"))
  '(or :bibtex :latex))
 
