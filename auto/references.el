73cd4f30e1ffeaf0bbe143b85a3d065a3ef0256f
Bruno Rodrigues <bruno.rodrigues@mesr.etat.lu>
2023-06-01 20:23:55 +0200

    typos formatting

--- auto/references.el
+++ auto/references.el
@@ -4,6 +4,7 @@
    (LaTeX-add-bibitems
     "knuth84"
     "chambers2014"
+    "vallet2022"
     "wickham2019"
     "leisch2002"
     "xie2014"
@@ -14,6 +15,10 @@
     "hammant2020"
     "trisovic2022"
     "peng2011"
-    "schoch2023"))
+    "schoch2023"
+    "rochette2022"
+    "landau2021"
+    "wickham2019b"
+    "firke2023"))
  '(or :bibtex :latex))
 
